365a39114d26d276b9b27e5a03630cf5b9e68aaa
tkm_mkzk <t.m.08013440879@icloud.com>
2022-03-19 16:46:46 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const increment = () => {
+  {
+    type: INCREMENT;
+  }
+};
+
+const decrement = () => {
+  {
+    type: DECREMENT;
+  }
+};
